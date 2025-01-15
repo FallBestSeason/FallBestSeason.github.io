e68e3241354b0f78ce05c2321db7a241593137a8
autumn <autumnday2020@gmail.com>
2025-01-15 12:48:55 -0500

    updated resume

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -6,13 +6,13 @@
       <div class="cItem">Github: <a href="https://github.com/FallBestSeason">FallBestSeason</a></div>
       <div class="cItem hContainer">Resume: 
         <div class="hItem">
-          <a href="/resume2025.pdf" target="_blank" rel="noopener noreferrer">.PDF </a>
+          <a href="/resume2025-public.pdf" target="_blank" rel="noopener noreferrer">.PDF </a>
         </div>
         <div class="hItem">
-          <a href="/resume2025.rtf" target="_blank" rel="noopener noreferrer">.RTF </a>
+          <a href="/resume2025-public.rtf" target="_blank" rel="noopener noreferrer">.RTF </a>
         </div>
         <div class="hItem">
-          <a href="/resume2025.docx" target="_blank" rel="noopener noreferrer">.DOCX</a>
+          <a href="/resume2025-public.docx" target="_blank" rel="noopener noreferrer">.DOCX</a>
         </div>
       </div>
     </div>
